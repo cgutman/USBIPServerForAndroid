9d8e3328c12a81140311c09f791ac15f0ea6f6cd
Cameron Gutman <aicommander@gmail.com>
2014-07-26 18:07:51 -0700

    Add new icons

--- gen/org/cgutman/usbipserverforandroid/R.java
+++ gen/org/cgutman/usbipserverforandroid/R.java
@@ -1073,6 +1073,7 @@ containing a value of this type.
         public static final int abc_textfield_searchview_right_holo_dark=0x7f020055;
         public static final int abc_textfield_searchview_right_holo_light=0x7f020056;
         public static final int ic_launcher=0x7f020057;
+        public static final int notification_icon=0x7f020058;
     }
     public static final class id {
         public static final int action_bar=0x7f05001c;
