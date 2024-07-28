06cf5ef9796c31b0457b80efa1cb4cc63c3ac3cb
Cameron Gutman <aicommander@gmail.com>
2024-07-28 14:52:38 -0500

    Update to NDK r27 with 16KB page support

--- app/src/main/jni/Application.mk
+++ app/src/main/jni/Application.mk
@@ -1,10 +1,13 @@
 # Application.mk for USB/IP Server
 
-# Our minimum version is Android 3.1
-APP_PLATFORM := android-12
+# Our minimum version is Android 5.0
+APP_PLATFORM := android-21
 
 # Build for all ABIs
 APP_ABI := all
 
 # We want an optimized build
 APP_OPTIM := release
+
+# We support 16KB pages
+APP_SUPPORT_FLEXIBLE_PAGE_SIZES := true
